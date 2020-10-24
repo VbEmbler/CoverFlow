edeb1d07353bc5bef22510df55522aa055bcb807
Embler <vbembler@gmail.com>
2020-10-24 12:42:04 +0300

    Initial Commit

---
+++ CoverFlow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoverFlow
+//
+//  Created by Vladimir on 24/10/2020.
+//  Copyright © 2020 Embler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
